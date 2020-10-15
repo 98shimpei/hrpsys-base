c2f82ebea0bd609fead5047bcc836f1e665949ec
98shimpei <satoshin.980608@gmail.com>
2020-10-15 11:01:09 +0900

    add box_pose from camera

--- rtc/AutoBalancer/AutoBalancer.h
+++ rtc/AutoBalancer/AutoBalancer.h
@@ -170,6 +170,8 @@ class AutoBalancer
   InPort<OpenHRP::TimedLandingPosition> m_landingHeightIn;
   OpenHRP::TimedSteppableRegion m_steppableRegion;
   InPort<OpenHRP::TimedSteppableRegion> m_steppableRegionIn;
+  OpenHRP::TimedQuatPose m_boxPose;
+  InPort<OpenHRP::TimedQuatPose> m_boxPoseIn;
 
   // for debug
   TimedPoint3D m_cog;
